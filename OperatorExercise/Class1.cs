890453e844107d04a1d43eb44acd06a97709d53f
jared mckemy <jaredmkcemy4@gmail.com>
2023-08-28 12:31:38 -0700

    Operator variables

---
+++ OperatorExercise/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    internal class Circle
+    {
+        public static double AreaOfCircle(double radius)
+        {
+             radius = (Math.PI * radius * radius);
+            return radius;
+        }
+
+    }
+}
--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,49 @@
 ﻿namespace OperatorExercise
 {
-    public class Program
+    internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 100;
+            int b = 2;
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % ++b);
+
+            Console.WriteLine("Please input a number to be used as the radius.");
+            double o = double.Parse(Console.ReadLine());
+            Console.WriteLine(Circle.AreaOfCircle(o));
         }
     }
 }
+
+
+
+
+//public static int Add(params int[] numbers)
+//    {
+//        int add = 0;
+//        foreach (int num in numbers) { add += num; }
+//        return add;
+//    }
+//public static int Subtract(int initial, params int [] numbers)
+//    {
+//        int subtract = initial;
+//        foreach (int num in numbers) { subtract -= num; }
+//        return subtract;
+//    }
+//    public static int Mulitply(int initial, params int[] numbers)
+//    {
+//        int multiply = initial;
+//        foreach (int num in numbers) { multiply *= num; }
+//        return multiply; 
+//    }
+//    public static int Divide(int intitial, params int[] numbers)
+//    {
+//        int divide = intitial;
+//        foreach (int num in numbers) { divide /= num; }
+//        return divide;
+//    }
